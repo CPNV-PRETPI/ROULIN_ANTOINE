50900f67d6dce71081c1e86a64790c2ba937b14b
Antoine <Antoine.ROULIN@cpnv.ch>
2023-02-21 10:38:07 +0100

    Intégration de l'action register et displayRegister dans l'index, index.php

--- index.php
+++ index.php
@@ -6,7 +6,25 @@
  * @version   10.02.2023
  */
 
+require "controller/userController.php";
 require "controller/navigation.php";
 
-home();
+if(isset($_GET['action'])){
+    $action = $_GET['action'];
+    switch ($action){
+        case "home":
+            home();
+            break;
+        case "displayRegister":
+            displayRegister();
+            break;
+        case "register":
+            register($_POST);
+            break;
+    }
+}
+else{
+    home();
+}
+
 
