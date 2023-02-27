75037f84b04d129071feb8ca645b86ce028256a6
Antoine <Antoine.ROULIN@cpnv.ch>
2023-02-27 10:17:50 +0100

    Création et ajouts des fichiers de test unitaire

---
+++ tests/unit/testBudget.php
@@ -0,0 +1,16 @@
+<?php
+
+class testBudget extends \PHPUnit\Framework\TestCase
+{
+    public function testBudgetAdd(){
+        $this->assertEquals(True, false);
+    }
+
+    public function testBudgetDelete(){
+        $this->assertEquals(True, false);
+    }
+
+    public function testBudgetReal(){
+        $this->assertEquals(True, false);
+    }
+}
---
+++ tests/unit/testUser.php
@@ -0,0 +1,110 @@
+<?php
+
+require "../../controller/userController.php";
+
+
+class testUser extends \PHPUnit\Framework\TestCase
+{
+    public function testUserRegisterFull(){
+
+        // create the post request array
+        $_POST['userEmail'] = 'unittest@test.ch';
+        $_POST['userUsername'] = 'unittest';
+        $_POST['userPassword'] = '1234';
+        $_POST['userPasswordVerify'] = '1234';
+
+        // register the new user with the array
+        register($_POST);
+
+        // check if the created user exists
+        try{
+            ifMemberExist($_POST['userEmail']);
+            // the user doesn't exists in the database
+            $isUserExists = false;
+        }
+        catch(registeredException){
+            // the user exists in the database
+            $isUserExists = true;
+        }
+        catch(databaseException){
+            $isUserExists = false;
+        }
+
+        $this->assertEquals(True, $isUserExists);
+
+        // clean
+        $this->cleanUser();
+    }
+
+    public function testUserRegisterPasswordNotTheSame(){
+
+        // create the post request array
+        $_POST['userEmail'] = 'unittest@test.ch';
+        $_POST['userUsername'] = 'unittest';
+        $_POST['userPassword'] = '1234';
+        $_POST['userPasswordVerify'] = 'paslememe';
+
+        // register the new user with the array
+        register($_POST);
+
+        // check if the created user exists
+        try{
+            ifMemberExist($_POST['userEmail']);
+            // the user doesn't exists in the database
+            $isUserExists = false;
+        }
+        catch(registeredException){
+            // the user exists in the database
+            $isUserExists = true;
+        }
+        catch(databaseException){
+            $isUserExists = false;
+        }
+
+        $this->assertEquals(False, $isUserExists);
+
+        // clean
+        $this->cleanUser();
+    }
+
+    public function testUserLogin(){
+
+        // create user
+
+        // login the user with user / password
+
+        // check if the session exists
+
+        $this->assertEquals(True, false);
+    }
+
+    public function testUserLogout(){
+
+        // create user
+
+        // login the user with user / password
+
+        // check if session exists
+
+        // logout
+
+        // check is session ended
+
+        $this->assertEquals(True, false);
+    }
+
+    public function testUserUnregister(){
+        $this->assertEquals(True, false);
+    }
+
+    public function cleanUser(){
+        require_once "../../model/dbConnector.php";
+        try {
+            $query = "DELETE FROM accounts WHERE email_address = 'unittest@test.ch'";
+            executeQuery($query);
+        }
+        catch (databaseException){
+            print 'Database error';
+        }
+    }
+}
