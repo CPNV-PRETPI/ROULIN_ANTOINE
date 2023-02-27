7e11227641fba15fa28690187fcea5a380c699eb
Antoine <Antoine.ROULIN@cpnv.ch>
2023-02-27 11:19:52 +0100

    Ajout d'un texte dans home.php après le register avec la valeur de la variable username dans la session

--- view/home.php
+++ view/home.php
@@ -4,6 +4,9 @@
 echo "Hello World !";
 ?>
 <a href="../index.php?action=displayRegister"><button>S'inscrire</button></a>
+<?php if(isset($_SESSION['userUsername'])):?>
+    <p><?= $_SESSION['userUsername'];?></p>
+<?php endif;?>
 </body>
 </html>
 
