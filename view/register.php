be14754fd77b3243119322ea37aebd3d0bd85396
Antoine <Antoine.ROULIN@cpnv.ch>
2023-02-27 10:30:51 +0100

    Ajout d'une balise texte avec un condition d'affichage qui affiche l'erreur utilisateur à l'utilisateur en cas d'erreur de register

--- view/register.php
+++ view/register.php
@@ -7,16 +7,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
+<?php if(isset($error)):?>
+    <p><?= $error;?></p>
+<?php endif;?>
 <div class="container">
         <h1 class="my-5"><b>Sign in</b></h1>
-        <form method="post" action="../index.php?action=register">
+        <form method="POST" action="../index.php?action=register">
             <div class="mb-3">
-                <label for="Username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="Username" placeholder="Type your username here" aria-describedby="emailHelp">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="userEmail" placeholder="Type your email here" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="userUsername" placeholder="Type your username here">
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Type your password here">
+                <input type="password" class="form-control" id="password" name="userPassword" placeholder="Type your password here">
+            </div>
+            <div class="mb-3">
+                <label for="passwordVerify" class="form-label">Password verify</label>
+                <input type="password" class="form-control" id="passwordVerify" name="userPasswordVerify" placeholder="Type your password again here">
             </div>
             <button type="submit" class="btn btn-primary">Sign in</button>
             <a href="../index.php?action=home"><button type="button" class="btn btn-danger">Cancel</button></a>
