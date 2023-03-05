8bc30255f2b67357174c698071fc6b7832cac126
Antoine <Antoine.ROULIN@cpnv.ch>
2023-03-05 14:16:49 +0100

    Remplacer la variable registerData par userTestData, cela me permettra de faire un setup avec tout les informations d'un utilisateur et de utiliser cette variable autant pour le register que le login

--- tests/unit/testUserModel.php
+++ tests/unit/testUserModel.php
@@ -12,108 +12,115 @@ require "../../model/userModel.php";
 
 class testUserModel extends TestCase
 {
-    private array $registerData = [];
-
+    private array $userTestData = [];
+    private array $loginData = [];
     public function setUp(): void
     {
-        $this->registerData['userEmail'] = 'unittest@test.ch';
-        $this->registerData['userUsername'] = 'unittest';
-        $this->registerData['userPassword'] = '1234';
-        $this->registerData['userPasswordVerify'] = '1234';
+        $this->userTestData['userEmail'] = 'unittest@test.ch';
+        $this->userTestData['userUsername'] = 'unittest';
+        $this->userTestData['userPassword'] = '1234';
+        $this->userTestData['userPasswordVerify'] = '1234';
     }
 
     public function testCheckData_DataMeetDatabaseExpectation_Success(){
         //Given
         //When
         //Then
-        $this->assertTrue(checkData($this->registerData));
+        $this->assertTrue(checkData($this->userTestData));
     }
 
     public function testCheckData_DataDoesntMeetDatabaseExpectation_Success(){
         //Given
-        $this->registerData['userUsername'] = '5JeJMu3kn3JHgApatT9YqyUjCMPD7PaE7aycDhtRdnzQPtqBad212'; //Username of exactly 53 char, database expect max of 50 char
+        $this->userTestData['userUsername'] = '5JeJMu3kn3JHgApatT9YqyUjCMPD7PaE7aycDhtRdnzQPtqBad212'; //Username of exactly 53 char, database expect max of 50 char
         //When
         //Then
-        $this->assertFalse(checkData($this->registerData));
+        $this->assertFalse(checkData($this->userTestData));
     }
 
     public function testCheckPasswordMatching_TwoPasswordMatch_Success(){
         //Given
         //When
         //Then
-        $this->assertTrue(checkPasswordMatching($this->registerData));
+        $this->assertTrue(checkPasswordMatching($this->userTestData));
     }
 
     public function testCheckPasswordMatching_TwoPasswordDoesntMatch_Success(){
         //Given
-        $this->registerData['userPasswordVerify'] = '5678'; //Password not match the password given in the SetUp function
+        $this->userTestData['userPasswordVerify'] = '5678'; //Password not match the password given in the SetUp function
         //When
         //Then
-        $this->assertFalse(checkPasswordMatching($this->registerData));
+        $this->assertFalse(checkPasswordMatching($this->userTestData));
     }
 
     public function testDoesMemberExist_UserDoesntExist_Success(){
         //Given
         //When
         //Then
-        $this->assertFalse(doesMemberExist($this->registerData));
+        $this->assertFalse(doesMemberExist($this->userTestData));
     }
 
     public function testDoesMemberExist_UserAlreadyExist_Success(){
         //Given
-        addUser($this->registerData);
+        addUser($this->userTestData);
         //When
         //Then
-        $this->assertTrue(doesMemberExist($this->registerData['userEmail']));
+        $this->assertTrue(doesMemberExist($this->userTestData['userEmail']));
     }
 
     public function testAddUser_NominalCase_Success(){
         //Given
         //When
-        addUser($this->registerData);
+        addUser($this->userTestData);
         //Then
-        $this->assertTrue(doesMemberExist($this->registerData['userEmail']));
+        $this->assertTrue(doesMemberExist($this->userTestData['userEmail']));
     }
 
     public function testRegister_NominalCase_Success(){
         //Given
         //When
-        register($this->registerData);
+        register($this->userTestData);
         //Then
-        $this->assertTrue(doesMemberExist($this->registerData['userEmail']));
+        $this->assertTrue(doesMemberExist($this->userTestData['userEmail']));
     }
 
     public function testRegister_UserAlreadyExist_ThrowException(){
         //Given
-        addUser($this->registerData);
+        addUser($this->userTestData);
         //When
         //Then
         $this->expectException(memberAlreadyExist::class);
-        register($this->registerData);
+        register($this->userTestData);
     }
 
     public function testRegister_UserFormNotMeetDataBaseRequirement_ThrowException(){
         //Given
-        $this->registerData['userUsername'] = '5JeJMu3kn3JHgApatT9YqyUjCMPD7PaE7aycDhtRdnzQPtqBad212'; //Username of exactly 53 char, database expect max of 50 char
+        $this->userTestData['userUsername'] = '5JeJMu3kn3JHgApatT9YqyUjCMPD7PaE7aycDhtRdnzQPtqBad212'; //Username of exactly 53 char, database expect max of 50 char
         //When
         //Then
         $this->expectException(notMeetDatabaseRequirement::class);
-        register($this->registerData);
+        register($this->userTestData);
     }
 
     public function testRegister_UserTwoPasswordDoesntMatch_ThrowException(){
         //Given
-        $this->registerData['userPasswordVerify'] = '5678'; //Password not match the password given in the SetUp function
+        $this->userTestData['userPasswordVerify'] = '5678'; //Password not match the password given in the SetUp function
         //When
         //Then
         $this->expectException(twoPasswordDontMatch::class);
-        register($this->registerData);
+        register($this->userTestData);
+    }
+
+    public function testLogin_NominalCase_Success(){
+        //Given
+        register($this->userTestData);
+        //When
+        //Then
     }
 
     public function cleanUser(){
         require_once "../../model/dbConnector.php";
         try {
-            $query = "DELETE FROM accounts WHERE email ='" . $this->registerData['userEmail'] ."';";
+            $query = "DELETE FROM accounts WHERE email ='" . $this->userTestData['userEmail'] ."';";
             executeQuery($query);
         }
         catch (databaseException){
@@ -124,7 +131,7 @@ class testUserModel extends TestCase
     public function tearDown(): void
     {
         // clean
-        if (doesMemberExist($this->registerData["userEmail"])){
+        if (doesMemberExist($this->userTestData["userEmail"])){
             $this->cleanUser();
         }
     }
