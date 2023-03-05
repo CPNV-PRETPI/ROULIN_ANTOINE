3922d1b8783af444e1b24348a77aa0e447eb1d97
Antoine <Antoine.ROULIN@cpnv.ch>
2023-03-05 10:53:12 +0100

    Ajout de la fonction login, elle n'est pas encore finie car tout les catchs n'ont pas encore été fait

--- controller/userController.php
+++ controller/userController.php
@@ -7,7 +7,7 @@
 */
 
 /**
- * @brief This function is designed to redirect the user on the register page and check if user : fill correctly all field, email entered by user doesn't match with a email user already registered and if all is ok it register the new member in the database
+ * @brief This function is designed to¨ check if user : fill correctly all field, email entered by user doesn't match with a email user already registered and if all is ok it register the new member in the database
  * @param $registerData
  * @return void
  */
@@ -34,3 +34,29 @@ function registerUser($registerData) : void
         require_once (dirname(__FILE__)."/../view/register.php");
     }
 }
+
+/**
+ * @brief This function is designed to login the user if : the email and password given by the user match with a user already registered
+ * @param $loginData
+ * @return void
+ */
+function loginUser($loginData) : void
+{
+    if(isset($loginData['userEmail']) && isset($loginData['userPassword'])){
+        try {
+            require_once dirname(__FILE__)."/../model/userModel.php";
+            login($loginData);
+            $_SESSION['userUsername'] = $loginData['userUsername'];
+            require_once (dirname(__FILE__)."/../view/home.php");
+        }
+        catch (databaseException $e){
+            $error = $e->getMessage(); //Set the variable $error by the message contained in the thrown exception to display after the content of $error in the view
+            require_once (dirname(__FILE__)."/../view/register.php");
+        }
+    } else {
+        $error = "One of the fields is empty, please fill it in"; //Set the variable $error a custom message if the form was not filled in completly
+        require_once (dirname(__FILE__)."/../view/register.php");
+    }
+}
+
+
