34340fee234f48a8e7f0d096800cddec600b60d8
Antoine <Antoine.ROULIN@cpnv.ch>
2023-03-06 12:24:51 +0100

    Minor fix

--- view/home.php
+++ view/home.php
@@ -4,6 +4,7 @@
 echo "Hello World !";
 ?>
 <a href="../index.php?action=displayLogin"><button>Login</button></a>
+<a href="../index.php?action=displayRegister"><button>Register</button></a>
 <?php if(isset($_SESSION['username'])):?>
     <p><?= $_SESSION['username'];?></p>
     <a href="../index.php?action=logout"><button>Logout</button></a>
