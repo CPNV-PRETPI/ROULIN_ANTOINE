c81db6094fd0eaa7784e28b667efcdaeb779c4ea
Antoine <Antoine.ROULIN@cpnv.ch>
2023-03-14 10:38:51 +0100

    core: Simplification du fonctionnement du dbConnector.php

--- model/dbConnector.php
+++ model/dbConnector.php
@@ -3,75 +3,40 @@
  * @file      dbConnector.php
  * @brief     This file is the model is used to connect to database and do request to the database
  * @author    Created by Antoine Roulin
- * @version   05.03.2023
+ * @version   14.03.2023
  */
 
 /**
  * @brief This function is designed to open and return a database connection
  * @return PDO
- * @throws DatabaseException
+ * @throws jsonFileException
  */
 function openDBConnexion() : PDO
 {
-    $tempDBConnexion = null;
-
     require_once dirname(__FILE__)."/jsonModel.php";
-    try{
-        $credentials = readJson(dirname(__FILE__)."/dbCredentials.json");
-        $dsn = $credentials->sqlDriver. ":host=". $credentials->hostName . ";dbname=".
-            $credentials->dbName.";port=".$credentials->port.";charset=".$credentials->charset;
-        $tempDBConnexion = new PDO($dsn, $credentials->userName, $credentials->userPwd);
-    }
-    catch(jsonFileException | PDOException){
-        throw new DatabaseException("An error has occurred, please try again later");
-    }
-    return $tempDBConnexion;
+
+    $credentials = readJson(dirname(__FILE__)."/dbCredentials.json");
+
+    $dsn = $credentials->sqlDriver. ":host=". $credentials->hostName . ";dbname=".
+        $credentials->dbName.";port=".$credentials->port.";charset=".$credentials->charset;
+
+    return new PDO($dsn, $credentials->userName, $credentials->userPwd);
 }
 
 /**
  * @brief This function is designed to execute a select query received as parameter
- * @param $query : the query to execute
+ * @param $query
  * @return array|null
- * @throws DatabaseException
- * @link https://php.net/manual/en/pdo.prepare.php
+ * @throws jsonFileException
  */
 function executeQuery($query) : array|null
 {
-    $queryResult = null;
     $dbConnexion = openDBConnexion();
-    if ($dbConnexion != null){
-        $statement = $dbConnexion->prepare($query);
-        $statement->execute();
-        $queryResult = $statement->fetchAll();
-    }
-    else{
-        throw new DatabaseException("An error has occurred, please try again later");
-    }
+
+    $statement = $dbConnexion->prepare($query);
+    $statement->execute();
+    $queryResult = $statement->fetchAll();
+
     $dbConnexion = null;
     return $queryResult;
 }
-
-/**
- * @brief This function is designed to execute an insert query received as parameter
- * @param $query
- * @return void
- * @throws DatabaseException
- */
-/*function executeQuery($query) : void
-{
-    $dbConnexion = openDBConnexion();
-    $result = false;
-    if ($dbConnexion != null){
-        $statement = $dbConnexion->prepare($query);
-        $result = $statement->execute();
-        if(!$result){
-            throw new DatabaseException("An error has occurred, please try again later");
-        }
-    }
-    else{
-        throw new DatabaseException("An error has occurred, please try again later");
-    }
-    $dbConnexion = null;
-}*/
-
-class DatabaseException extends Exception{}
