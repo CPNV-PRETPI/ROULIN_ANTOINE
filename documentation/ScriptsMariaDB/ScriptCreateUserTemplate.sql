d3ed561e3792a796b123a2be773617446afd66c8
Antoine <Antoine.ROULIN@cpnv.ch>
2023-03-17 12:03:27 +0100

    doc: Add sql script template to create user database for the webapp

---
+++ documentation/ScriptsMariaDB/ScriptCreateUserTemplate.sql
@@ -0,0 +1,3 @@
+DROP USER IF EXISTS [to replace]@[to replace];
+CREATE USER '[to replace]'@'[to replace]' IDENTIFIED BY '[to replace]';
+GRANT SELECT, INSERT, UPDATE, DELETE ON [to replace].[to replace] TO '[to replace]'@'[to replace]';
