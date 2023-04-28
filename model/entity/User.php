143881a544dfc210c95d2c4a2bb05e65d552bed8
Antoine <Antoine.ROULIN@cpnv.ch>
2023-04-28 11:55:35 +0200

    feat: add comment to entity user

--- model/entity/User.php
+++ model/entity/User.php
@@ -5,20 +5,35 @@
  * @author    Created by Antoine Roulin
  * @version   28.04.2023
  */
-
 class User
 {
     private string $email;
     private string $password;
+
+    /**
+     * @brief User constructor.
+     * @param $email
+     * @param $password
+     */
     public function __construct($email, $password)
     {
         $this->email = $email;
         $this->password = $password;
     }
+
+    /**
+     * @brief Get the email of the user.
+     * @return string
+     */
     public function getEmail(): string
     {
         return $this->email;
     }
+
+    /**
+     * @brief Get the password of the user.
+     * @return string
+     */
     public function getPassword(): string
     {
         return $this->password;
